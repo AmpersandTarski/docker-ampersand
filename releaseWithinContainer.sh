f556ba1930599314eab609966d03fd4214fb98e9
Stef Joosten <stef.joosten@users.noreply.github.com>
2017-07-04 10:59:43 +0200

    fix: trying to automate release cycle.

--- releaseWithinContainer.sh
+++ releaseWithinContainer.sh
@@ -3,12 +3,7 @@
 # Postcondition: the latest release of RAP3 from repo https://github.com/AmpersandTarski/ampersand-models/RAP3 is in production.
 # All student scripts are being preserved.
 
-#  echo "creating a backup of scripts and data from RAP3"
-. backup.sh
-
-# reminder for Stef: docker exec -ti dockerampersand_rap3_1 /bin/bash
-
-# echo "creating new instance of RAP3"
+echo "creating new instance of RAP3"
 # tactic: first make a new instance called RAPgen, then release it by flipping directories.
 # The existing instance of RAP3 is preserved in RAPprev until the following release.
 rm -rf /var/www/html/RAPprev
