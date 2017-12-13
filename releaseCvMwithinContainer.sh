5e26e20ce5062a9d63dfad6babf097e6642907ca
Hidde-Jan Jongsma <hidde-jan.jongsma@tno.nl>
2017-12-13 15:29:54 +0100

    Cleanup

--- releaseCvMwithinContainer.sh
+++
@@ -1,7 +0,0 @@
-# Run this script to overwrite CvM with its latest release. 
-
-echo "creating new instance of CvM"
-cd ~/git/Ampersand-models
-git pull
-ampersand --verbose -p/var/www/html/CvM CvM/CvM.adl
-chown -R www-data:www-data /var/www/html/CvM
